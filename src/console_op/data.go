861643010ffd38fb830fd8854cfcf41a98eabfbd
David Laine <dlaine@cray.com>
2021-04-08 14:34:25 -0500

    Initial code for console-operator.

---
+++ src/console_op/data.go
@@ -0,0 +1,262 @@
+/*
+ * MIT License
+ *
+ * (C) Copyright [2021] Hewlett Packard Enterprise Development LP
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// This file contains the code needed to interact with the console-data
+//  service.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Variable to hold address of console-data service
+var dataAddrBase string = "http://cray-console-data/v1"
+
+// function to interact with console-data api to add new noded to the db
+func dataAddNodes(newNodes []nodeConsoleInfo) bool {
+	// return if there was a successful response from console-data
+	retVal := false
+
+	// Just log a summary
+	log.Printf("Sending %d nodes to console-data", len(newNodes))
+
+	// NOTE: data is just a simple array of nodeConsoleInfo structs - no packaging
+	data, err := json.Marshal(newNodes)
+	if err != nil {
+		log.Printf("Error marshalling data for add nodes:%s", err)
+		return retVal
+	}
+
+	// use 'PUT' to get into data service
+	URL := dataAddrBase + "/inventory"
+	rd, rc, err := putURL(URL, data, nil)
+	if err != nil {
+		log.Printf("Error adding new data to console-data inventory: %s", err)
+		return retVal
+	}
+
+	// log if call succeeded (anything less than http 400 is success)
+	retVal = rc < 400
+
+	// decode the response
+	type response struct {
+		message string
+	}
+	rp := response{}
+	err = json.Unmarshal(rd, &rp)
+	if err != nil {
+		// handle error
+		// TODO - better error handling?  Do we need a retry so if something fails
+		//  it won't get out of sync??
+		log.Printf("Error unmarshalling data: %s, bytesArray:%s", err, rd)
+	} else {
+		log.Printf("Console-data return message: %s", rp.message)
+	}
+	return retVal
+}
+
+// function to interact with console-data api to remove existing nodes from the db
+func dataRemoveNodes(removedNodes []nodeConsoleInfo) {
+	// NOTE: data is just a simple array of nodeConsoleInfo structs - no packaging
+	data, err := json.Marshal(removedNodes)
+	if err != nil {
+		log.Printf("Error marshalling data for remove nodes:%s", err)
+		return
+	}
+
+	// dump input to log
+	log.Printf("Nodes removing from console-data:")
+	for _, ni := range removedNodes {
+		log.Printf("  Node: %s", ni.NodeName)
+	}
+
+	// use 'DELETE' to get into data service
+	URL := dataAddrBase + "/inventory"
+	rd, rc, err := deleteURL(URL, data, nil)
+	if err != nil {
+		log.Printf("Unable to remove elements from console-data: %s", err)
+		return
+	}
+
+	if rd != nil {
+		// decode the response
+		type response struct {
+			message string
+		}
+		rp := response{}
+		err = json.Unmarshal(rd, &rp)
+		if err != nil {
+			// handle error
+			// TODO - better error handling?  Do we need a retry so if something fails
+			//  it won't get out of sync??
+			log.Printf("Error unmarshalling data: %s", err)
+		} else {
+			log.Printf("Console-data return message: %s", rp.message)
+		}
+	} else {
+		log.Printf("Console-data had no return data, response code:%d", rc)
+	}
+
+}
+
+// trigger a clearing of nodes from a stale pod
+func checkHeartbeats() {
+	// set the stale time to be 3 min
+	const staleMinutes int = 3
+
+	// Check for stale heartbeats every 15 sec
+	const checkHeartbeatsSec int = 15
+
+	for {
+		// format the url for the clear API
+		url := fmt.Sprintf("%s/consolepod/%d/clear", dataAddrBase, staleMinutes)
+
+		// call the console-data api
+		_, _, err := deleteURL(url, nil, nil)
+		if err != nil {
+			log.Printf("Error calling console-data clear stale heartbeats:%s", err)
+		}
+
+		// wait for the next interval
+		time.Sleep(time.Duration(checkHeartbeatsSec) * time.Second)
+
+	}
+}
+
+// GetNodePodResponse - used to report service health stats
+type GetNodePodResponse struct {
+	PodName string `json:"podname"`
+}
+
+// GetNodeData - input data for call to getNodeData
+type GetNodeData struct {
+	XName string `json:"xname"`
+}
+
+// BaseResponse - error message for a bad response
+type BaseResponse struct {
+	Msg string `json:"message"`
+}
+
+// Get which pod a particular console is connected to
+func doGetNodePod(w http.ResponseWriter, r *http.Request) {
+	// NOTE: this is provided as a quick check of the internal status for
+	//  administrators to aid in determining the health of this service.
+
+	// only allow 'GET' calls
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// read the request data - must be in json content
+	reqBody, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Printf("There was an error reading the request body: S%s\n", err)
+		var body = BaseResponse{
+			Msg: fmt.Sprintf("There was an error reading the request body: S%s", err),
+		}
+		SendResponseJSON(w, http.StatusBadRequest, body)
+		return
+	}
+	contentType := r.Header.Get("Content-type")
+	log.Printf("Content-Type: %s\n", contentType)
+	if contentType != "application/json" {
+		var body = BaseResponse{
+			Msg: fmt.Sprintf("Expecting Content-Type: application/json"),
+		}
+		SendResponseJSON(w, http.StatusBadRequest, body)
+		return
+	}
+	log.Printf("request data: %s\n", string(reqBody))
+
+	var inData GetNodeData
+	err = json.Unmarshal(reqBody, &inData)
+	if err != nil {
+		log.Printf("There was an error while decoding the json data: %s\n", err)
+		var body = BaseResponse{
+			Msg: fmt.Sprintf("There was an error while decoding the json data: %s", err),
+		}
+		SendResponseJSON(w, http.StatusBadRequest, body)
+		return
+	}
+
+	// get the correct pod from the console-data service
+	podName, err := getNodePodForXname(inData.XName)
+	if err != nil {
+		log.Printf("Error getting console node pod from console-data: %s", err)
+		var body = BaseResponse{
+			Msg: fmt.Sprintf("There was an error querying console-data service: %s", err),
+		}
+		SendResponseJSON(w, http.StatusInternalServerError, body)
+		return
+	}
+
+	// package and return the value
+	var res GetNodePodResponse
+	res.PodName = podName
+	SendResponseJSON(w, http.StatusOK, res)
+	return
+}
+
+// query the console-data service for the correct pod
+func getNodePodForXname(xname string) (string, error) {
+	// now we have the name the user is looking for, put the request to console-data
+	url := fmt.Sprintf("%s/consolepod/%s", dataAddrBase, xname)
+	rd, _, err := getURL(url, nil)
+	if err != nil {
+		log.Printf("Error getting console node pod from console-data: %s", err)
+		return "", err
+	}
+
+	// pull the data from the return package
+	type RetNodeConsoleInfo struct {
+		NodeName        string `json:"nodename"`        // node xname
+		BmcName         string `json:"bmcname"`         // bmc xname
+		BmcFqdn         string `json:"bmcfqdn"`         // full name of bmc
+		Class           string `json:"class"`           // river/mtn class
+		NID             int    `json:"nid"`             // NID of the node
+		Role            string `json:"role"`            // role of the node
+		NodeConsoleName string `json:"nodeconsolename"` // the pod console
+	}
+
+	var nd RetNodeConsoleInfo
+	err = json.Unmarshal(rd, &nd)
+	if err != nil {
+		log.Printf("Error unmashalling data from console-data: %s", err)
+		return "", err
+	}
+
+	// return the result
+	return fmt.Sprintf("cray-console-node-%s", nd.NodeConsoleName), nil
+}
---
+++ src/console_op/health.go
@@ -0,0 +1,342 @@
+/*
+ * MIT License
+ *
+ * (C) Copyright [2021] Hewlett Packard Enterprise Development LP
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// This file contains the health endpoint functions
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// HealthResponse - used to report service health stats
+type HealthResponse struct {
+	NumberConsoles       string `json:"consoles"`
+	LastHardwareUpdate   string `json:"hardwareupdate"`
+	NumberNodePods       string `json:"nodepods"`
+	NumberRvrNodesPerPod string `json:"rvrnodesperpod"`
+	NumberMtnNodesPerPod string `json:"mtnnodesperpod"`
+	MaxRvrNodesPerPod    string `json:"maxrvrnodesperpod"`
+	MaxMtnNodesPerPod    string `json:"maxmtnnodesperpod"`
+}
+
+// Function to output contents of the struct as a string
+func (hr HealthResponse) String() string {
+	return fmt.Sprintf("NumConsoles: %s, LastUpdate: %s, NumNodePods: %s, NumRvrNodesPerPod: %s, NumMtnNodesPerPod: %s, MaxRiverNodesPerPod: %s, MaxMtnNodesPerPod: %s",
+		hr.NumberConsoles, hr.LastHardwareUpdate, hr.NumberNodePods, hr.NumberRvrNodesPerPod, hr.NumberMtnNodesPerPod, hr.MaxRvrNodesPerPod, hr.MaxMtnNodesPerPod)
+}
+
+// Basic liveness probe
+func doHealth(w http.ResponseWriter, r *http.Request) {
+	// NOTE: this is provided as a quick check of the internal status for
+	//  administrators to aid in determining the health of this service.
+
+	// only allow 'GET' calls
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// get the current health status
+	stats := getCurrentHealth()
+
+	// log the query
+	log.Printf("Health check: %s", stats)
+
+	// write the output
+	SendResponseJSON(w, http.StatusOK, stats)
+	return
+}
+
+// Fill out the current status of a HealthResponse object
+func getCurrentHealth() HealthResponse {
+	var stats HealthResponse
+	stats.LastHardwareUpdate = hardwareUpdateTime
+	stats.NumberConsoles = fmt.Sprintf("%d", len(nodeCache))
+	stats.NumberNodePods = fmt.Sprintf("%d", numNodePods)
+	stats.NumberRvrNodesPerPod = fmt.Sprintf("%d", numRvrNodesPerPod)
+	stats.NumberMtnNodesPerPod = fmt.Sprintf("%d", numMtnNodesPerPod)
+	stats.MaxRvrNodesPerPod = fmt.Sprintf("%d", maxRvrNodesPerPod)
+	stats.MaxMtnNodesPerPod = fmt.Sprintf("%d", maxMtnNodesPerPod)
+	return stats
+}
+
+// Basic liveness probe
+func doLiveness(w http.ResponseWriter, r *http.Request) {
+	// NOTE: this is coded in accordance with kubernetes best practices
+	//  for liveness/readiness checks.  This function should only be
+	//  used to indicate the server is still alive and processing requests.
+
+	log.Printf("Doing liveness check")
+
+	// only allow 'GET' calls
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// return simple StatusOK response to indicate server is alive
+	w.WriteHeader(http.StatusNoContent)
+	return
+}
+
+// Basic readiness probe
+func doReadiness(w http.ResponseWriter, r *http.Request) {
+	// NOTE: this is coded in accordance with kubernetes best practices
+	//  for liveness/readiness checks.  This function should only be
+	//  used to indicate the server is still alive and processing requests.
+
+	log.Printf("Doing readiness check")
+
+	// only allow 'GET' calls
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// return simple StatusOK response to indicate server is alive
+	w.WriteHeader(http.StatusNoContent)
+	return
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Added some debug endpoints below for useful testing / probing of live
+//  systems.  They are not documented, but are present.
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// MaxNodeData - Simple struct to return error information
+type MaxNodeData struct {
+	MaxRvrNodes int `json:"maxRvr"` // max number of river nodes per pod
+	MaxMtnNodes int `json:"maxMtn"` // max number of mountain nodes per pod
+}
+
+// small helper function to insure correct number of nodes asked for
+func pinNumNodes(numAsk, numMin, numMax int) (int, bool) {
+	// insure the input number ends in range [0,numMax]
+	ok := true
+	val := numAsk
+	if val < numMin {
+		// already have too many
+		val = numMin
+		ok = false
+	} else if val > numMax {
+		// pin at the maximum
+		val = numMax
+		ok = false
+	}
+	return val, ok
+}
+
+// Debugging information probe
+func doSetMaxNodesPerPod(w http.ResponseWriter, r *http.Request) {
+	// API to set the max number of nodes per pod
+	log.Printf("Call to setMaxNodesPerPod...")
+
+	// only allow 'PATCH' calls
+	if r.Method != http.MethodPatch {
+		w.Header().Set("Allow", "PATCH")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// read the request data - must be in json content
+	reqBody, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Printf("There was an error reading the request body: S%s\n", err)
+		var body = BaseResponse{
+			Msg: fmt.Sprintf("There was an error reading the request body: S%s", err),
+		}
+		SendResponseJSON(w, http.StatusBadRequest, body)
+		return
+	}
+	contentType := r.Header.Get("Content-type")
+	log.Printf("Content-Type: %s\n", contentType)
+	if contentType != "application/json" {
+		var body = BaseResponse{
+			Msg: fmt.Sprintf("Expecting Content-Type: application/json"),
+		}
+		SendResponseJSON(w, http.StatusBadRequest, body)
+		return
+	}
+	log.Printf("request data: %s\n", string(reqBody))
+
+	var inData MaxNodeData
+	err = json.Unmarshal(reqBody, &inData)
+	if err != nil {
+		log.Printf("There was an error while decoding the json data: %s\n", err)
+		var body = BaseResponse{
+			Msg: fmt.Sprintf("There was an error while decoding the json data: %s", err),
+		}
+		SendResponseJSON(w, http.StatusBadRequest, body)
+		return
+	}
+
+	// process the results - do a sanity check on the user input
+	log.Printf("Resetting max nodes based on user input: maxMtn: %d, maxRvr: %d", inData.MaxMtnNodes, inData.MaxRvrNodes)
+	ok := true
+	maxMtnNodesPerPod, ok = pinNumNodes(inData.MaxMtnNodes, 2, 750)
+	if !ok {
+		log.Printf("Error - invalid max mountain nodes per pod. Asked: %d, defaulted to: %d",
+			inData.MaxMtnNodes, maxMtnNodesPerPod)
+	}
+	maxRvrNodesPerPod, ok = pinNumNodes(inData.MaxRvrNodes, 2, 2000)
+	if !ok {
+		log.Printf("Error - invalid max river nodes per pod. Asked: %d, defaulted to: %d",
+			inData.MaxRvrNodes, maxRvrNodesPerPod)
+	}
+
+	// write the response
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+// NodePodPair - information for which console-node pod an xname is controlled by
+type NodePodPair struct {
+	PodID    string
+	NumNodes int
+}
+
+// InfoResponse - package of debug data for export
+type InfoResponse struct {
+	Nodes  []NodePodPair
+	Health HealthResponse
+}
+
+// Debugging information probe
+func doInfo(w http.ResponseWriter, r *http.Request) {
+	// NOTE: this is provided as a quick check of the internal status for
+	//  administrators to aid in determining the health of this service.
+
+	// only allow 'GET' calls
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", "GET")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// fill in health response portion
+	var info InfoResponse
+	info.Health = getCurrentHealth()
+
+	// keep track of how many nodes are connected to each node-pod
+	tally := make(map[string]int)
+	for nn := range nodeCache {
+		podName, err := getNodePodForXname(nn)
+		if err != nil {
+			tally["Unassigned"] = tally["Unassigned"] + 1
+		} else {
+			tally[podName] = tally[podName] + 1
+		}
+	}
+
+	// package into the return response
+	for k, v := range tally {
+		info.Nodes = append(info.Nodes, NodePodPair{PodID: k, NumNodes: v})
+	}
+
+	// write the response
+	SendResponseJSON(w, http.StatusOK, info)
+	return
+}
+
+// Debugging only - clear all current data from services
+func doClearData(w http.ResponseWriter, r *http.Request) {
+	// NOTE: this is provided as a quick check of the internal status for
+	//  administrators to aid in determining the health of this service.
+	log.Printf("Calling doClearData...")
+
+	// only allow 'DELETE' calls
+	if r.Method != http.MethodDelete {
+		w.Header().Set("Allow", "DELETE")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// get the pod each node is in and remove from console-data
+	var rn []nodeConsoleInfo = make([]nodeConsoleInfo, 0, len(nodeCache))
+	for _, ni := range nodeCache {
+		rn = append(rn, ni)
+	}
+	nodeCache = make(map[string]nodeConsoleInfo)
+	dataRemoveNodes(rn)
+
+	// write the response
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+// Debugging only - suspend querying the state manager
+func doSuspend(w http.ResponseWriter, r *http.Request) {
+	// only allow 'POST' calls
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// HACK - if we set the 'inShutdown' flag to true it will prevent actions
+	inShutdown = true
+
+	log.Printf("Updates suspended")
+	// write the response
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+// Debugging only - resume querying the state manager
+func doResume(w http.ResponseWriter, r *http.Request) {
+	// only allow 'POST' calls
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		sendJSONError(w, http.StatusMethodNotAllowed,
+			fmt.Sprintf("(%s) Not Allowed", r.Method))
+		return
+	}
+
+	// HACK - if we set the 'inShutdown' flag to true it will prevent actions
+	inShutdown = false
+
+	log.Printf("Updates resumed")
+
+	// write the response
+	w.WriteHeader(http.StatusOK)
+	return
+}
